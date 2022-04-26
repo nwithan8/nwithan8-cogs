7a1614548cc15496a42571cc3ab937949962c923
Nate Harris <nharris@easypost.com>
2022-04-25 23:43:40 -0600

    - Add support for multiple user queues in Q - Begin support for item queues in Q

--- q/database.py
+++ q/database.py
@@ -11,9 +11,11 @@ class QueueEntry:
     __tablename__ = "q"
     timestamp = db.Column("timestamp", db.DateTime, nullable=False)
     processed = db.Column("processed", db.Boolean, nullable=False)
+    queue_name = db.Column("queue_name", db.String(255), nullable=False)
 
     @db.none_as_null
-    def __init__(self, timestamp: datetime.datetime = None, processed: bool = False, **kwargs):
+    def __init__(self, queue_name: str, timestamp: datetime.datetime = None, processed: bool = False, **kwargs):
+        self.queue_name = queue_name
         self.timestamp = timestamp or kwargs.get('timestamp') or datetime.datetime.now()
         self.processed = processed or kwargs.get('processed') or False
 
@@ -23,8 +25,8 @@ class UserQueueEntry(QueueEntry, Base):
     user_id = db.Column("user_id", db.String(1000), primary_key=True, nullable=False)
 
     @db.none_as_null
-    def __init__(self, user_id: int = None, **kwargs):
-        super().__init__()
+    def __init__(self, queue_name: str, user_id: int = None, **kwargs):
+        super().__init__(queue_name=queue_name)
         self.user_id = f"{user_id}" if user_id else kwargs.get('user_id', None)
 
 
@@ -33,8 +35,8 @@ class ItemQueueEntry(QueueEntry, Base):
     item_value = db.Column("item_value", db.String(1000), primary_key=True, nullable=False)
 
     @db.none_as_null
-    def __init__(self, item_value: str = None, **kwargs):
-        super().__init__()
+    def __init__(self, queue_name: str, item_value: str = None, **kwargs):
+        super().__init__(queue_name=queue_name)
         self.item_value = item_value if item_value else kwargs.get('item_value', None)
 
 
@@ -45,41 +47,44 @@ class QueueDatabase(db.SQLAlchemyDatabase):
         super().__init__(sqlite_file=sqlite_file, table_schemas=table_schemas)
 
     @db.table_exists(table_name='user_queue')
-    def get_next_user_from_queue(self) -> Union[UserQueueEntry, None]:
+    def get_next_user_from_queue(self, queue_name: str) -> Union[UserQueueEntry, None]:
         """
-        Get the next user from the q
+        Get the next user from the queue
 
         :return:
         """
-        return self.get_first_by_filters(table=UserQueueEntry, processed=False,
+        return self.get_first_by_filters(table=UserQueueEntry, processed=False, queue_name=queue_name,
                                          order=arcadb.expression.desc(UserQueueEntry.timestamp))
 
     @db.table_exists(table_name='user_queue')
-    def add_user_to_queue(self, user_id: int) -> None:
+    def add_user_to_queue(self, queue_name: str, user_id: int) -> None:
         """
-        Add a user to the q
+        Add a user to the queue
 
+        :param queue_name:
         :param user_id:
         :return:
         """
-        user_queue_entry = self.create_entry_if_does_not_exist(table_schema=UserQueueEntry, fields_to_check=["user_id"],
-                                                               user_id=f"{user_id}")
+        user_queue_entry = self.create_entry_if_does_not_exist(table_schema=UserQueueEntry,
+                                                               fields_to_check=["queue_name", "user_id"],
+                                                               queue_name=f"{queue_name}", user_id=f"{user_id}")
         if user_queue_entry is False:
             return None
         return user_queue_entry
 
     @db.table_exists(table_name='user_queue')
-    def find_user_location_in_queue(self, user_id: int) -> Union[int, None]:
+    def find_user_location_in_queue(self, queue_name: str, user_id: int) -> Union[int, None]:
         """
-        Find the location of a user in the q
+        Find the location of a user in the queue
 
+        :param queue_name:
         :param user_id:
         :return:
         """
         # get all unprocessed entries
         unprocessed_entries = self.get_all_by_filters(table=UserQueueEntry,
                                                       order=arcadb.expression.desc(UserQueueEntry.timestamp),
-                                                      processed=False)
+                                                      queue_name=f"{queue_name}", processed=False)
         counter = 1
         for entry in unprocessed_entries:
             if entry.user_id == f'{user_id}':
@@ -88,93 +93,98 @@ class QueueDatabase(db.SQLAlchemyDatabase):
         return None  # user not found
 
     @db.table_exists(table_name='user_queue')
-    def get_user_from_queue(self, user_id: int) -> Union[UserQueueEntry, None]:
+    def get_user_from_queue(self, queue_name: str, user_id: int) -> Union[UserQueueEntry, None]:
         """
-        Get a user from the q
+        Get a user from the queue
 
+        :param queue_name:
         :param user_id:
         :return:
         """
-        return self.get_first_by_filters(table=UserQueueEntry, user_id=f'{user_id}')
+        return self.get_first_by_filters(table=UserQueueEntry, queue_name=f'{queue_name}', user_id=f'{user_id}')
 
     @db.table_exists(table_name='user_queue')
-    def update_user_status_in_queue(self, user_id: int, status: bool):
+    def update_user_status_in_queue(self, queue_name: str, user_id: int, status: bool):
         """
-        Update the status of a user in the q
+        Update the status of a user in the queue
 
+        :param queue_name:
         :param user_id:
         :param status:
         :return:
         """
-        user_queue_entry = self.get_first_by_filters(table=UserQueueEntry, user_id=f'{user_id}')
+        user_queue_entry = self.get_first_by_filters(table=UserQueueEntry,
+                                                     queue_name=f'{queue_name}', user_id=f'{user_id}')
         if user_queue_entry:
             user_queue_entry.Processed = status
             self.commit()
 
     @db.table_exists(table_name='user_queue')
-    def get_all_unprocessed_users(self) -> List[UserQueueEntry]:
+    def get_all_unprocessed_users(self, queue_name: str) -> List[UserQueueEntry]:
         """
-        Get all unprocessed users
+        Get all unprocessed users in the queue
 
         :return:
         """
-        return self.get_all_by_filters(table=UserQueueEntry, Processed=False)
+        return self.get_all_by_filters(table=UserQueueEntry, processed=False, queue_name=f'{queue_name}')
 
     @db.table_exists(table_name='user_queue')
-    def get_all_processed_users(self) -> List[UserQueueEntry]:
+    def get_all_processed_users(self, queue_name: str) -> List[UserQueueEntry]:
         """
-        Get all processed users
+        Get all processed users in the queue
 
         :return:
         """
-        return self.get_all_by_filters(table=UserQueueEntry, Processed=True)
+        return self.get_all_by_filters(table=UserQueueEntry, processed=True, queue_name=f'{queue_name}')
 
     @db.table_exists(table_name='user_queue')
-    def get_all_users(self) -> List[UserQueueEntry]:
+    def get_all_users(self, queue_name: str) -> List[UserQueueEntry]:
         """
-        Get all users
+        Get all users in the queue
 
         :return:
         """
-        return self.get_all_by_filters(table=UserQueueEntry)
+        return self.get_all_by_filters(table=UserQueueEntry, queue_name=f'{queue_name}')
 
     @db.table_exists(table_name='user_queue')
-    def delete_user_from_queue(self, user_id: int) -> bool:
+    def delete_user_from_queue(self, queue_name: str, user_id: int) -> bool:
         """
-        Delete a user from the q
+        Delete a user from the queue
 
+        :param queue_name:
         :param user_id:
         :return:
         """
-        return self.delete_by_filters(table=UserQueueEntry, user_id=f'{user_id}')
+        return self.delete_by_filters(table=UserQueueEntry, user_id=f'{user_id}', queue_name=f'{queue_name}')
 
     @db.table_exists(table_name='user_queue')
-    def purge_users(self):
+    def purge_users(self, queue_name: str):
         """
-        Purge all processed users from the q
+        Purge all processed users from the queue
 
         :return:
         """
-        for user in self.get_all_processed_users():
+        for user in self.get_all_processed_users(queue_name=queue_name):
             user.delete()
             self.commit()
 
     @db.table_exists(table_name='user_queue')
-    def mass_remove_users(self, user_ids: List[int]):
+    def mass_remove_users(self, queue_name: str, user_ids: List[int]):
         """
-        Mass remove users from the q
+        Mass remove users from the queue
 
+        :param queue_name:
         :param user_ids:
         :return:
         """
         for user_id in user_ids:
-            self.delete_user_from_queue(user_id)
+            self.delete_user_from_queue(queue_name=queue_name, user_id=user_id)
             self.commit()
 
     @db.table_exists(table_name='user_queue')
     def export_user_queue_to_csv(self, csv_file: str) -> bool:
         """
-        Export all users from the queue to a csv file
+        Export all users from all queues to a csv file
 
         :param csv_file:
         :return:
@@ -184,7 +194,7 @@ class QueueDatabase(db.SQLAlchemyDatabase):
     @db.table_exists(table_name='item_queue')
     def export_item_queue_to_csv(self, csv_file: str) -> bool:
         """
-        Export all items from the queue to a csv file
+        Export all items from all queues to a csv file
 
         :param csv_file:
         :return:
--- q/queue.py
+++ q/queue.py
@@ -21,140 +21,149 @@ class Queue(BaseCog):
         self.config = Config.get_conf(self, identifier=IDENTIFIER, force_registration=True)
 
         default_guild = {
-            "queue_types": []
+            "queue_names_and_types": {}
         }
 
         self.config.register_guild(**default_guild)
 
-    async def add_queue_type(self, ctx: Context, queue_type: QueueType) -> None:
-        existing_queue_types = await self.config.guild(ctx.guild).queue_types()
-        existing_queue_types.append(queue_type.string)
-        await self.config.guild(ctx.guild).queue_types.set(existing_queue_types)
-        await ctx.send("Queue type set up!")
+    async def add_queue(self, ctx: Context, queue_name: str, queue_type: QueueType) -> None:
+        existing_queue_settings = await self.config.guild(ctx.guild).queue_names_and_types()
+        existing_queue_settings[queue_name] = queue_type.string
+        await self.config.guild(ctx.guild).queue_names_and_types.set(existing_queue_settings)
+        await ctx.send(f"{queue_type.string} queue ``{queue_name}`` set up!")
 
     async def get_database(self, ctx: Context) -> QueueDatabase:
         guild_database_path = get_guild_database(guild=ctx.guild)
-        queue_type_strings = await self.config.guild(ctx.guild).queue_types()
+        queue_settings = await self.config.guild(ctx.guild).queue_names_and_types()
         schemas = []
-        for queue_type_string in queue_type_strings:
+        for queue_name, queue_type_string in queue_settings.items():
             queue_type = QueueType.from_string(queue_type_string)
-            if queue_type:
+            if queue_type and queue_type not in schemas:
                 schemas.append(queue_type.schema)
         return QueueDatabase(sqlite_file=guild_database_path, table_schemas=schemas)
 
-    async def queue_set_up(self, ctx: Context, queue_type: QueueType) -> bool:
-        return queue_type.string in await self.config.guild(ctx.guild).queue_types()
+    async def queue_set_up(self, ctx: Context, queue_name: str, queue_type: QueueType) -> bool:
+        settings = await self.config.guild(ctx.guild).queue_names_and_types()
+        return queue_name in settings.keys() and settings[queue_name] == queue_type.string
 
-    async def database_pre_check(self, ctx: Context, queue_type: QueueType) -> bool:
-        if not await self.queue_set_up(ctx=ctx, queue_type=queue_type):
-            await ctx.send("Queue type is not set up!")
+    async def database_pre_check(self, ctx: Context, queue_name: str, queue_type: QueueType) -> bool:
+        if not await self.queue_set_up(ctx=ctx, queue_name=queue_name, queue_type=queue_type):
+            await ctx.send(f"{queue_type.string} queue ``{queue_name}`` is not set up!")
             return False
         return True
+
+    async def create_new_queue(self, ctx: Context, queue_name: str, queue_type: QueueType) -> None:
+        if await self.queue_set_up(ctx=ctx, queue_name=queue_name, queue_type=queue_type):
+            await ctx.send(f"{queue_type.string} queue `{queue_name}` is already set up!")
+            return
+        await self.add_queue(ctx=ctx, queue_name=queue_name, queue_type=queue_type)
 
     @commands.guild_only()
     @commands.group(name="q")
     async def queue(self, ctx):
         pass
 
-    @queue.command(name="new")
-    @checks.admin_or_permissions(manage_guild=True)
-    async def queue_new(self, ctx: Context, queue_type: str):
-        """
-        Creates a new q
-        """
-        queue_type_enum = QueueType.from_string(queue_type)
-        if not queue_type_enum:
-            await ctx.send("Invalid queue type!")
-            return
-        if queue_type_enum.string in await self.config.guild(ctx.guild).queue_types():
-            await ctx.send("Queue type is already set up!")
-            return
-        await self.add_queue_type(ctx=ctx, queue_type=queue_type_enum)
-
     @queue.group(name="users")
     async def queue_users(self, ctx: Context):
         pass
 
+    @queue_users.command(name="new")
+    async def queue_users_new(self, ctx: Context, queue_name: str):
+        """
+        Creates a new users queue
+        """
+        await self.create_new_queue(ctx=ctx, queue_name=queue_name, queue_type=QueueType.USERS)
+
     @queue_users.command(name="add")
-    async def queue_users_add(self, ctx: Context):
+    async def queue_users_add(self, ctx: Context, queue_name: str):
         """
-        Adds yourself to the q.
+        Adds yourself to the queue
         """
-        if await self.database_pre_check(ctx=ctx, queue_type=QueueType.USERS):
+        if await self.database_pre_check(ctx=ctx, queue_name=queue_name, queue_type=QueueType.USERS):
             user_id = ctx.author.id
             database = await self.get_database(ctx=ctx)
-            if database.get_user_from_queue(user_id=user_id):
-                await ctx.send("You are already in the queue!")
+            if database.get_user_from_queue(queue_name=queue_name, user_id=user_id):
+                await ctx.send(f"You are already in the `{queue_name}` queue!")
                 return
-            if database.add_user_to_queue(user_id=user_id):
-                await ctx.send("You have been added to the queue!")
+            if database.add_user_to_queue(queue_name=queue_name, user_id=user_id):
+                await ctx.send(f"You have been added to the `{queue_name}` queue!")
                 return
             await self.send_error(ctx=ctx)
 
     @queue_users.command(name="remove")
-    async def queue_users_remove(self, ctx: Context, user: discord.Member = None):
+    async def queue_users_remove(self, ctx: Context, queue_name: str, user: discord.Member = None):
         """
-        Remove yourself or another user from the q.
+        Remove yourself or another user from the queue
         """
-        if await self.database_pre_check(ctx=ctx, queue_type=QueueType.USERS):
+        if await self.database_pre_check(ctx=ctx, queue_name=queue_name, queue_type=QueueType.USERS):
             database = await self.get_database(ctx=ctx)
             if user:
                 if not await is_mod_or_superior(self.bot, ctx.author):
                     await self.send_error(ctx=ctx,
-                                          error_message="You do not have permission to remove other users from the "
-                                                        "queue!")
+                                          error_message=f"You do not have permission to remove other users from the "
+                                                        f"`{queue_name}` queue!")
                     return
                 user_id = user.id
-                if not database.get_user_from_queue(user_id=user_id):
-                    await ctx.send("That user is not in the queue!")
+                if not database.get_user_from_queue(queue_name=queue_name, user_id=user_id):
+                    await ctx.send(f"That user is not in the `{queue_name}` queue!")
                     return
-                database.delete_user_from_queue(user_id=user_id)  # always returns true
-                await ctx.send(f"<@{user_id}> has been removed from the queue!")
+                database.delete_user_from_queue(queue_name=queue_name, user_id=user_id)  # always returns true
+                await ctx.send(f"<@{user_id}> has been removed from the `{queue_name}` queue!")
             else:
                 user_id = ctx.author.id
-                if not database.get_user_from_queue(user_id=user_id):
-                    await ctx.send("You are not in the queue!")
+                if not database.get_user_from_queue(queue_name=queue_name, user_id=user_id):
+                    await ctx.send(f"You are not in the `{queue_name}` queue!")
                     return
-                database.delete_user_from_queue(user_id=user_id)  # always returns true
-                await ctx.send(f"You has been removed from the queue!")
+                database.delete_user_from_queue(queue_name=queue_name, user_id=user_id)  # always returns true
+                await ctx.send(f"You has been removed from the `{queue_name}` queue!")
 
     @queue_users.command(name="place")
-    async def queue_users_place(self, ctx: Context):
+    async def queue_users_place(self, ctx: Context, queue_name: str):
         """
-        See where you are in the q
+        See where you are in the queue
         """
-        if await self.database_pre_check(ctx=ctx, queue_type=QueueType.USERS):
+        if await self.database_pre_check(ctx=ctx, queue_name=queue_name, queue_type=QueueType.USERS):
             database = await self.get_database(ctx=ctx)
             user_id = ctx.author.id
-            user_location = database.find_user_location_in_queue(user_id=user_id)
+            user_location = database.find_user_location_in_queue(queue_name=queue_name, user_id=user_id)
             if not user_location:
-                await ctx.send("You are not in the queue!")
+                await ctx.send(f"You are not in the `{queue_name}`queue!")
                 return
-            await ctx.send(f"You are currently {int_to_place(user_location)} in the queue!")
+            await ctx.send(f"You are currently {int_to_place(user_location)} in the `{queue_name}` queue!")
 
     @queue_users.command(name="next")
     @checks.admin_or_permissions(manage_guild=True)
-    async def queue_users_next(self, ctx: Context):
+    async def queue_users_next(self, ctx: Context, queue_name: str):
         """
-        See who is next in the q
+        See who is next in the queue
         """
-        if await self.database_pre_check(ctx=ctx, queue_type=QueueType.USERS):
+        if await self.database_pre_check(ctx=ctx, queue_name=queue_name, queue_type=QueueType.USERS):
             database = await self.get_database(ctx=ctx)
-            next_user = database.get_next_user_from_queue()
+            next_user = database.get_next_user_from_queue(queue_name=queue_name)
             if not next_user:
-                await ctx.send("There is no one in the queue!")
+                await ctx.send(f"There is no one in the `{queue_name}` queue!")
                 return
-            await ctx.send(f"The next user in the queue is <@{next_user.user_id}>!")
+            await ctx.send(f"The next user in the `{queue_name}` queue is <@{next_user.user_id}>!")
 
     @queue_users.command(name="export")
     @checks.admin_or_permissions(manage_guild=True)
     async def queue_users_export(self, ctx: Context):
         """
-        Export the queue to a CSV.
+        Export all queues to a CSV.
         """
-        if await self.database_pre_check(ctx=ctx, queue_type=QueueType.USERS):
-            database = await self.get_database(ctx=ctx)
-            if not database.export_user_queue_to_csv(f"{ctx.guild.id}_user_queue.csv"):
-                await self.send_error(ctx=ctx)
-                return
-            await ctx.send(file=discord.File(f"{ctx.guild.id}_user_queue.csv"))
+        database = await self.get_database(ctx=ctx)
+        if not database.export_user_queue_to_csv(f"{ctx.guild.id}_user_queue.csv"):
+            await self.send_error(ctx=ctx)
+            return
+        await ctx.send(file=discord.File(f"{ctx.guild.id}_user_queue.csv"))
+
+    @queue.group(name="items")
+    async def queue_items(self, ctx: Context):
+        pass
+
+    @queue_items.command(name="new")
+    async def queue_items_new(self, ctx: Context, queue_name: str):
+        """
+        Creates a new items queue
+        """
+        await self.create_new_queue(ctx=ctx, queue_name=queue_name, queue_type=QueueType.ITEMS)
